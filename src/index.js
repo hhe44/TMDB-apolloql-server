500199a9817de500a21422445403787e17c46241
unknown <hhe@mailfence.com>
2020-02-09 14:34:32 -0500

    dotenv and movie genres configured

--- src/index.js
+++ src/index.js
@@ -1,53 +1,50 @@
+require("dotenv").config();
 const { ApolloServer, gql } = require("apollo-server");
-const MovieAPI = require('./datasources/movie');
+const MovieAPI = require("./datasources/movie");
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
 const typeDefs = gql`
-
   type Query {
     movie(id: ID!): Movie
   }
 
   type Movie {
-    adult: Boolean,
-    backdrop_path: String,
-    budget: Int,
-    homepage: String,
-    id: ID!,
-    imdb_id: String,
-    original_language: String,
-    original_title: String,
-    overview: String,
-    popularity: Float,
-    poster_path: String,
-    release_date: String,
-    revenue: Int,
-    runtime: Int,
-    status: String,
-    tagline: String,
-    title: String,
-    video: Boolean,
-    vote_average: Float,
+    adult: Boolean
+    backdrop_path: String
+    budget: Int
+    genre: Genre
+    homepage: String
+    id: ID!
+    imdb_id: String
+    original_language: String
+    original_title: String
+    overview: String
+    popularity: Float
+    poster_path: String
+    release_date: String
+    revenue: Int
+    runtime: Int
+    status: String
+    tagline: String
+    title: String
+    video: Boolean
+    vote_average: Float
     vote_count: Int
   }
 
+  type Genre {
+    id: ID!
+    name: String
+  }
 `;
 
 const dataSources = () => ({
   movieAPI: new MovieAPI()
 });
-
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    movie: (_, { id }, { dataSources }) =>
-      dataSources.movieAPI.getMovie({ id }),
+    movie: (_, { id }, { dataSources }) => dataSources.movieAPI.getMovie({ id })
   }
 };
-
 
 const server = new ApolloServer({ typeDefs, resolvers, dataSources });
 
